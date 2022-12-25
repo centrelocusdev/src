0c7ccf2b8fab1257513600c823f6d118faf7e7a7
rupali-codes <rupali7487@gmail.com>
2022-12-25 23:56:15 +0530

    working on pages

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Dashboard = () => {
+    return (
+        <div className="text-center text-gray-400 text-6xl mt-5">Coming soon</div>
+    )
+}
+
+export default Dashboard
