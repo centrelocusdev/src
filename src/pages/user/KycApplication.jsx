27bac8a42108d5ee16ac6d8db7389426c4dca2fc
Rupali Haldiya <rupali7487@gmail.com>
2023-02-10 22:17:29 +0530

    fixing tabs for kyc form

--- src/pages/user/KycApplication.jsx
+++ src/pages/user/KycApplication.jsx
@@ -1,8 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { toast } from "react-toastify";
 import ButtonPrimary from "../../components/ButtonPrimary";
 import InputPrimary from "../../components/InputPrimary";
+import { kycForm } from "../../utils/api";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
 const KycApplication = () => {
+  const [formData, setFormData] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    phone_number: "",
+    dob: "",
+    address_line_1: "",
+    address_line_2: "",
+    city: "",
+    state: "",
+    nationality: "",
+    zip_code: "",
+    national_id_image: "",
+  });
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const response = await kycForm(formData);
+    toast.success("KYC completed");
+  };
+
+  //tabs
+  const tabs = ["Personal Details", "Your Address", "Document Upload", "Finals"];
+  const [tab, setTab] = useState(tabs[0]);
+
+  const handleCurrentTab = (currentTab) => {
+    setTab(currentTab);
+  };
+
+  const onPressNext = () => {
+    const index = tabs.indexOf(tab)+1
+    console.log(index < tabs.length-1)
+    if(index <= tabs.length-1) {
+      setTab(tabs[index])
+    }
+    else {
+      setTab(tabs[0])
+    }
+  }
+
+  const onPressPrevious = () => {
+    const index = tabs.indexOf(tab)-1
+    console.log(index, tabs.length-1)
+    if(index >= 0) {
+      setTab(tabs[index])
+    }
+    else {
+      setTab(tabs[tabs.length-1])
+    }
+  }
+
   return (
     <>
       <section className="p-5 text-zinc-400">
@@ -14,146 +72,229 @@ const KycApplication = () => {
             Verify your identity to participate in token sale.
           </p>
         </div>
+
+        <nav className="flex justify-start md:gap-16 gap-7 text-xl font-semibold uppercase">
+          <button
+            onClick={(e) => handleCurrentTab(e.target.textContent)}
+            className={`mt-3 transition-all ${
+              tab === "Personal Details" && "text-yellow-500 border-yellow-500"
+            } pt-2 border-b-2 border-transparent text-gray-400 text-sm md:text-lg hover:text-yellow-500 hover:border-yellow-500`}
+          >
+            Personal Details
+          </button>
+          <button
+            onClick={(e) => handleCurrentTab(e.target.textContent)}
+            className={`mt-3 transition-all ${
+              tab === "Your Address" && "text-yellow-500 border-yellow-500"
+            } pt-2 border-b-2 border-transparent text-gray-400 text-sm md:text-lg hover:text-yellow-500 hover:border-yellow-500`}
+          >
+            Your Address
+          </button>
+          <button
+            onClick={(e) => handleCurrentTab(e.target.textContent)}
+            className={`mt-3 transition-all ${
+              tab === "Document Upload" && "text-yellow-500 border-yellow-500"
+            } pt-2 border-b-2 border-transparent text-gray-400 text-sm md:text-lg hover:text-yellow-500 hover:border-yellow-500`}
+          >
+            Document Upload
+          </button>
+          <button
+            onClick={(e) => handleCurrentTab(e.target.textContent)}
+            className={`mt-3 transition-all ${
+              tab === "Finals" && "text-yellow-500 border-yellow-500"
+            } pt-2 border-b-2 border-transparent text-gray-400 text-sm md:text-lg hover:text-yellow-500 hover:border-yellow-500`}
+          >
+            Finals
+          </button>
+        </nav>
+
         <form
           action=""
-          className="mt-3 rounded-xl shadow-lg border border-dashed border-gray-400 px-5"
+          className="mt-3 rounded-xl shadow-lg border border-dashed border-gray-400 p-5"
         >
-          <div className="py-3 bg-zinc-800 mt-6 px-6 rounded">
-            <h3 className="text-[24px] text-yellow-500">Personal Details</h3>
-            <p className="text-gray-500">
-            Please type carefully and fill out the form with your personal
-              details. Your can’t edit these details once you submitted the
-              form.
-            </p>
-          </div>
 
-          <div className="px-4">
-            <div className="mt-6 -mb-12 ">
-              <div className="lg:flex md:flex justify-between">
-                <InputPrimary
-                  field={"First name"}
-                  name={"firstName"}
-                  placeholder={"John"}
-                />
-                <InputPrimary
-                  field={"Last name"}
-                  name={"lastName"}
-                  placeholder={"Smith"}
-                />
-                <InputPrimary
-                  field={"Email address"}
-                  name={"email"}
-                  placeholder={"you@example.com"}
-                />
+          {tab === tabs[0] && (
+            <>
+              <p className="text-gray-500 pl-3 mt-5 border-l-4 border-gray-400">
+                Please type carefully and fill out the form with your personal
+                details. Your can’t edit these details once you submitted the
+                form.
+              </p>
+
+              <div className="px-4">
+                <div className="mt-6">
+                  <div className="lg:flex md:flex justify-between">
+                    <InputPrimary
+                      field={"First Name"}
+                      name={"first_name"}
+                      placeholder={"John"}
+                      value={formData.first_name}
+                    />
+                    <InputPrimary
+                      field={"Last Name"}
+                      name={"last_name"}
+                      placeholder={"Smith"}
+                      value={formData.last_name}
+                    />
+                  </div>
+
+                  <div className="lg:flex md:flex justify-between">
+                    <InputPrimary
+                      field={"Email Address"}
+                      name={"email"}
+                      placeholder={"you@example.com"}
+                      value={formData.email}
+                    />
+                    <InputPrimary
+                      field={"Phone Number"}
+                      name={"phone_number"}
+                      placeholder={" "}
+                      value={formData.phone_number}
+                    />
+                  </div>
+                  <div className="lg:w-[375px]">
+                    <InputPrimary
+                      field={"Date of Birth"}
+                      name={"dob"}
+                      placeholder={" "}
+                      type="date"
+                      value={formData.dob}
+                    />
+                  </div>
+                </div>
               </div>
-
-              <div className="lg:flex md:flex justify-between">
-              <InputPrimary
-                  field={"phone number"}
-                  name={"phoneNumber"}
-                  placeholder={" "}
-                />
-                <InputPrimary
-                  field={"Date of Birth"}
-                  name={"dateOfBirth"}
-                  placeholder={" "}
-                />
-                <InputPrimary
-                  field={"Telegram username"}
-                  name={"telegramUsername"}
-                  placeholder={" "}
-                />
-              </div>
-
+            </>
+          )}
+          {tab === tabs[1] && (
+            <>
+              <p className="text-gray-500 text-gray-500 pl-3 mt-5 border-l-4 border-gray-400">
+                We care about your privacy and your address won't be shared with anybody. Please enter the details carefully as we might need to contact you in future
+              </p>
               <div className="mt-3">
-                <h4 className="text-lg px-3 text-yellow-500 font-semibold">
-                  Your Address
-                </h4>
-
                 <div className="lg:flex md:flex justify-between">
                   <InputPrimary
                     field={"Address line 1"}
-                    name={"address1"}
+                    name={"address_line_1"}
                     placeholder={" "}
+                    value={formData.address_line_1}
                   />
                   <InputPrimary
                     field={"Address line 2"}
-                    name={"address2"}
+                    name={"address_line_2"}
                     placeholder={" "}
-                  />
-                   <InputPrimary
-                    field={"City"}
-                    name={"city"}
-                    placeholder={" "}
+                    value={formData.address_line_2}
                   />
                 </div>
 
                 <div className="lg:flex md:flex justify-between">
-                <InputPrimary
+                  <InputPrimary
+                    field={"City"}
+                    name={"city"}
+                    placeholder={" "}
+                    value={formData.city}
+                  />
+                  <InputPrimary
                     field={"State"}
                     name={"state"}
                     placeholder={" "}
+                    value={formData.state}
                   />
+                </div>
+                <div className="lg:flex md:flex justify-between">
                   <InputPrimary
                     field={"Nationality"}
                     name={"nationality"}
                     placeholder={" "}
+                    value={formData.nationality}
                   />
                   <InputPrimary
                     field={"Zip Code"}
-                    name={"zipCode"}
+                    name={"zip_code"}
                     placeholder={" "}
+                    value={formData.zip_code}
                   />
                 </div>
               </div>
-            </div>
-          </div>
-
-          <div className="bg-zinc-800 lg:px-12 px-6 py-6 mt-16 rounded">
-            <h3 className="text-[24px] text-yellow-500">Document Upload</h3>
-            <p className="text-gray-500">
-            In order to complete, please upload any of the following personal
-              document.
-            </p>
-          </div>
-
-          <div className="py-6">
-            <div className="lg:flex md:flex justify-between mt-3">
-              <InputPrimary
-                type={"file"}
-                field={"Passport"}
-                name={"passport"}
-                placeholder={" "}
-              />
-              <InputPrimary
-                type={"file"}
-                field={"National card"}
-                name={"nationalCard"}
-                placeholder={" "}
-              />
-              <InputPrimary
-                type={"file"}
-                field={"Driver's license"}
-                name={"driverLicense"}
-                placeholder={" "}
-              />
-            </div>
-
-            <div className="my-4">
-              <h3 className="text-yellow-500 font-semibold text-[18px]">
-                To avoid delays when verifying account, Please make sure bellow:
-              </h3>
-
-              <p className="my-1">
-                Chosen credential must not be expaired.
+            </>
+          )}
+          {tab === tabs[2] && (
+            <>
+              <p className="text-gray-500 text-gray-500 pl-3 mt-5 border-l-4 border-gray-400">
+                In order to complete your KYC, please upload any/all of the following
+                personal documents carefully
               </p>
-              <p className="my-1">
-                Document should be good condition and clearly visible.
+              <div className="py-6">
+                <div className="lg:flex md:flex justify-between mt-3">
+                  <InputPrimary
+                    type={"file"}
+                    field={"Passport"}
+                    name={"passport_image"}
+                    placeholder={" "}
+                    value={formData.passport_image}
+                  />
+                  <InputPrimary
+                    type={"file"}
+                    field={"National card"}
+                    name={"national_id_image"}
+                    placeholder={" "}
+                    value={formData.national_id_image}
+                  />
+                </div>
+                <div className="lg:w-[390px]">
+                  <InputPrimary
+                    type={"file"}
+                    field={"Driver's license"}
+                    name={"driver_license"}
+                    placeholder={" "}
+                    value={formData.driver_license}
+                  />
+                </div>
+
+                <div className="my-4">
+                  <h3 className="text-yellow-500 font-semibold text-[18px]">
+                    To avoid delays when verifying account, Please make sure
+                    bellow:
+                  </h3>
+
+                  <p className="my-1">
+                    Chosen credential must not be expaired.
+                  </p>
+                  <p className="my-1">
+                    Document should be good condition and clearly visible.
+                  </p>
+                  <p className="my-1">
+                    Make sure that there is no light glare on the card.
+                  </p>
+                </div>
+              </div>
+            </>
+          )}
+          {tab === tabs[3] && (
+            <>
+              <p className="text-gray-500 text-gray-500 pl-3 my-5 border-l-4 border-gray-400">
+                You almost reached to end of your KYC proccess. Please check the following feilds carefully
               </p>
-              <p className="my-1">
-                Make sure that there is no light glare on the card.
-              </p>
-            </div>
+              <div className="flex lg:px-13 mx-2">
+                <input type="checkbox" name={"privacyPolicy"} className="" />
+                <label htmlFor="privacyPolicy" className="mx-3">
+                  I Have Read The Terms Of Condition And Privary Policy.
+                </label>
+              </div>
+
+              <div className="flex lg:px-13 mx-2">
+                <input type="checkbox" name={"isCorrect"} className="" />
+                <label htmlFor="isCorrect" className="mx-3">
+                  All The Personal Information I Have Entered Is Correct.
+                </label>
+              </div>
+
+              <ButtonPrimary text={"Proccess to verify"} />
+            </>
+          )}
+
+          <div className="mt-5 w-full text-yellow-500 flex justify-between text-xl">
+              <FaArrowAltCircleLeft className="hover:text-yellow-200 cursor-pointer" title="Previous" onClick={onPressPrevious} />
+              <FaArrowAltCircleRight className="hover:text-yellow-200 cursor-pointer" title="Next" onClick={onPressNext} />
           </div>
 
           {/* <div className="bg-zinc-800 p-6 mt-5">
@@ -190,22 +331,6 @@ const KycApplication = () => {
               />
             </div>
           </div> */}
-
-          <div className="flex lg:px-13 mx-2">
-            <input type="checkbox" name={"privacyPolicy"} className="" />
-            <label htmlFor="privacyPolicy" className="mx-3">
-              I Have Read The Terms Of Condition And Privary Policy.
-            </label>
-          </div>
-
-          <div className="flex lg:px-13 mx-2">
-            <input type="checkbox" name={"isCorrect"} className="" />
-            <label htmlFor="isCorrect" className="mx-3">
-              All The Personal Information I Have Entered Is Correct.
-            </label>
-          </div>
-
-          <ButtonPrimary text={"Proccess to verify"} />
         </form>
       </section>
     </>
--- src/utils/api.js
+++ src/utils/api.js
@@ -4,123 +4,116 @@ import { toast } from "react-toastify";
 import Cookies from "js-cookie";
 import { api_url } from "./config";
 
+const headers = {
+  headers: {
+    Authorization: `Token ${Cookies.get("src-token")}`,
+  }
+}
+
 //send otp
 export const sendOtp = async (email) => {
-  const response = await axios.post(`${api_url}/get_otp/`, { email })
-  return response.data
-}
+  const response = await axios.post(`${api_url}/get_otp/`, { email });
+  return response.data;
+};
 
 //user register
 export const register = async (userData) => {
-  const response = await axios.post(
-    `${api_url}/registration/buyer/`,
-    userData
-  );
+  const response = await axios.post(`${api_url}/registration/buyer/`, userData);
 
   if (response.data) {
     Cookies.set("src-token", response.data.token);
-    toast.success('User registered successfully')
-    return response.data
-  } else if(response.data.Error) {
-    toast.error(response.data.Error)
+    toast.success("User registered successfully");
+    return response.data;
+  } else if (response.data.Error) {
+    toast.error(response.data.Error);
   }
 };
 
 //user login
 export const login = async (userData) => {
-  const response = await axios.post(
-    `${api_url}/login/`,
-    userData
-  );
+  const response = await axios.post(`${api_url}/login/`, userData);
 
-  if(response.data.Response) {
-    toast.error(response.data.Response)
-  }
-  else if (response.data) {
+  if (response.data.Response) {
+    toast.error(response.data.Response);
+  } else if (response.data) {
     Cookies.set("src-token", response.data.token);
-    toast.success('User logged in')
-    return response.data
+    toast.success("User logged in");
+    return response.data;
   }
 };
 
 //user logout
 export const logout = async () => {
-  Cookies.remove('src-token')
-  await axios.get(`${api_url}/logout/`)
-  toast.success('User logged out')
-  return true
+  Cookies.remove("src-token");
+  await axios.get(`${api_url}/logout/`);
+  toast.success("User logged out");
+  return true;
 };
 
 //change user password
 export const changePassword = async (passwords) => {
-  const response = await axios.put(`${api_url}/change-password/`, passwords, {
-    headers: {
-      Authorization: `Token ${Cookies.get('src-token')}`
-    }
-  })
-  toast.success('Password changed successfully')
+  const response = await axios.put(`${api_url}/change-password/`, passwords, headers);
+  toast.success("Password changed successfully");
 
-  return response.data
+  return response.data;
 };
 
 //send token for forgot password
 export const sendToken = async (email) => {
   const response = await axios.post(`${api_url}/password_reset/`, { email });
 
-  return response.data
-}
+  return response.data;
+};
 
 //forgot password
 export const forgotPassword = async (password, token) => {
-  const response = await axios.post(`${api_url}/password_reset/confirm/`, { password, token });
+  const response = await axios.post(`${api_url}/password_reset/confirm/`, {
+    password,
+    token,
+  });
 
-  return response.data
+  return response.data;
 };
 
-//admin login 
+//admin login
 export const adminLogin = async (email, password) => {
   try {
-
   } catch (err) {
-    toast.error(err)
+    toast.error(err);
   }
-}
+};
 
 //get user by token
 export const getCurrentUser = async () => {
-  const response = await axios.get(`${api_url}/current_user/`, {
-    headers: {
-      Authorization: `Token ${Cookies.get('src-token')}`
-    }
-  })
+  const response = await axios.get(`${api_url}/current_user/`, headers);
 
-  if(response.data) {
-    return response.data
+  if (response.data) {
+    return response.data;
   } else {
-    toast.error("couldn't get user")
+    toast.error("couldn't get user");
   }
-}
+};
 
 //get user profile details
 export const getUserDetails = async () => {
-  const response = await axios.get(`${api_url}/buyer_update/`, {
-    headers: {
-      Authorization: `Token ${Cookies.get('src-token')}`
-    }
-  })
+  const response = await axios.get(`${api_url}/buyer_update/`, headers);
 
-  return response.data
-}
+  return response.data;
+};
 
 //update user profile
 export const updateUserDetails = async (profileDetails) => {
-  const response = await axios.put(`${api_url}/buyer_update/`, profileDetails, {
-    headers: {
-      Authorization: `Token ${Cookies.get('src-token')}`
-    }
-  })
+  const response = await axios.put(
+    `${api_url}/buyer_update/`,
+    profileDetails,
+    {}
+  );
 
-  return response.data
+  return response.data;
+};
 
+//user kyc form
+export const kycForm = async (formData) => {
+  const response = await axios.put(`${api_url}/kyc/update/`, formData, headers);
   return response.data
-}
+};
