4a7b06b328c2519e4922610b8a317e4e483d22ef
Rupali Haldiya <rupali7487@gmail.com>
2023-03-17 20:13:42 +0530

    update base_url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 import Cookies from "js-cookie";
 
 export const token = Cookies.get('src-token')
-export const api_url = 'http://52.66.196.48:8000/api'
+// export const api_url = 'http://52.66.196.48:8000/api'
+export const api_url = 'https://dev.thedelvierypointe.com/api'
